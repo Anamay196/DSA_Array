e8b591ca03bccd938adce9336a530fcab3870484
Anamay Dubey <72741575+Anamay196@users.noreply.github.com>
2022-10-16 15:03:01 +0530

    Binary Search Algorithm (Iterative Method)
    Binary search algorithm can be applied only on sorted array.

---
+++ Binary Search Algorithm (Iterative Method).java
@@ -0,0 +1,39 @@
+/* BINARY SEARCH ALGORITHM (ONLY FOR SORTED ARRAY)
+Binary Search is a searching algorithm used in a sorted array by repeatedly 
+dividing the search interval in half. 
+The idea of binary search is to use the information that the array is sorted 
+and reduce the time complexity to O(Log n). 
+*/
+
+class Main{
+    
+    public static int binary_search(int arr[],int low,int high,int x)
+    {
+        while(low<=high) // repeat search until low=high
+        {
+            int mid = (low+high)/2;
+            if(x==arr[mid])
+                return mid; 
+            else if(x>arr[mid])
+                low = mid+1; // new low for divided array
+            else    
+                high = mid-1; // new high for divided array
+        }
+        return -1; // if element not found
+    }
+    public static void main(String[] args)
+    {
+        int arr[] = {12,34,45,67,89,112,119};
+        int x = 34;
+        int last = arr.length-1; // last index 
+        
+        int n = binary_search(arr,0,last,x);
+        if(n==-1)
+            System.out.println("Element not found");
+        else    
+            System.out.println("Element found at index: "+n);
+        
+        
+        
+    }
+}
